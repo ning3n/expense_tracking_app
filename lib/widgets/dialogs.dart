bf04d0d81d5810d7f8215ec51ca9fc5a52f04cdd
Miguel <ningenmr@gmail.com>
2025-06-11 18:53:22 -0400

    Added TapDialog witdget

--- lib/widgets/dialogs.dart
+++ lib/widgets/dialogs.dart
@@ -38,4 +38,56 @@ class _AddCategoryDialogState extends State<AddCategoryDialog> {
       ],
     );
   }
+
+  @override
+  void dispose(){
+    _nameController.dispose();
+    super.dispose();
+  }
+}
+
+class AddTapDialog extends StatefulWidget {
+  const AddTapDialog({super.key});
+
+  @override
+  State<AddTapDialog> createState() => _AddTapDialogState();
+}
+
+class _AddTapDialogState extends State<AddTapDialog> {
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Add Tag'),
+      content: TextField(
+        controller: _nameController,
+        decoration: InputDecoration(labelText: 'Tag Name'),
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          }, 
+          child: Text('Cancel'),
+        ),
+        ElevatedButton(
+          onPressed: (){
+            final tagName = _nameController.text;
+            if(tagName.isNotEmpty){
+              //Add tag logic here
+              Navigator.of(context).pop(tagName);
+            }
+          }, 
+          child: Text('Add'),
+        ),
+      ],
+    );
+  }
+
+  @override 
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
 }
