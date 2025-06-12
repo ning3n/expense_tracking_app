fc652c58717035d12aebc189360e43b735c34c00
Miguel <ningenmr@gmail.com>
2025-06-11 20:37:43 -0400

    Added final points

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MainApp());
+import 'package:localstorage/localstorage.dart';
+import 'package:provider/provider.dart';
+import 'models/expense_provider.dart';
+import 'screens/category_management_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/tag_management_screen.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initLocalStorage();
+  runApp(MyApp(localStorage: localStorage));
 }
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  final LocalStorage localStorage;
+  const MyApp({Key? key, required this.localStorage}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ExpenseProvider(localStorage)),
+      ],
+      child: MaterialApp(
+        title: 'Expense Tracker',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomeScreen(), // Main entry point, HomeScreen
+          '/manage_categories': (context) =>
+              CategoryManagementScreen(), // Route for managing categories
+          '/manage_tags': (context) =>
+              TagManagementScreen(), // Route for managing tags
+        },
+        // Removed 'home:' since 'initialRoute' is used to define the home route
       ),
     );
   }
